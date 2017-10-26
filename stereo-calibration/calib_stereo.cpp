99776424cd67c70b5615c5e83281e0308372c528
ttomchy <none@none.none>
2017-10-26 14:51:59 +0800

    simiulation_successful_calrbrate_stero_camera

---
+++ stereo-calibration/calib_stereo.cpp
@@ -0,0 +1,263 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <stdio.h>
+#include <iostream>
+#include "popt_pp.h"
+
+using namespace std;
+using namespace cv;
+
+vector< vector< Point3f > > object_points;
+vector< vector< Point2f > > imagePoints1, imagePoints2;
+vector< Point2f > corners1, corners2;
+vector< vector< Point2f > > left_img_points, right_img_points;
+
+Mat img1, img2, gray1, gray2;
+Mat image_cornors_l,image_cornors_r;
+void load_image_points(int board_width, int board_height, int num_imgs, float square_size,
+                      char* leftimg_dir, char* rightimg_dir, char* leftimg_filename, char* rightimg_filename) {
+
+  Size board_size = Size(board_width, board_height);
+  int board_n = board_width * board_height;
+
+  for (int i = 1; i <= num_imgs; i++) {
+    char left_img[100], right_img[100];
+    sprintf(left_img, "%s%s%d.jpg", leftimg_dir, leftimg_filename, i);
+    sprintf(right_img, "%s%s%d.jpg", rightimg_dir, rightimg_filename, i);
+    img1 = imread(left_img, CV_LOAD_IMAGE_COLOR);
+    img2 = imread(right_img, CV_LOAD_IMAGE_COLOR);
+    cvtColor(img1, gray1, CV_BGR2GRAY);
+    cvtColor(img2, gray2, CV_BGR2GRAY);
+
+    bool found1 = false, found2 = false;
+
+    found1 = cv::findChessboardCorners(img1, board_size, corners1,
+  CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FILTER_QUADS);
+    found2 = cv::findChessboardCorners(img2, board_size, corners2,
+  CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FILTER_QUADS);
+
+    if (found1)
+    {
+      cv::cornerSubPix(gray1, corners1, cv::Size(5, 5), cv::Size(-1, -1),
+  cv::TermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 30, 0.1));
+      cv::drawChessboardCorners(img1, board_size, corners1, found1);
+    }
+    if (found2)
+    {
+      cv::cornerSubPix(gray2, corners2, cv::Size(5, 5), cv::Size(-1, -1),
+  cv::TermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 30, 0.1));
+      cv::drawChessboardCorners(img2, board_size, corners2, found2);
+    }
+
+    vector< Point3f > obj;
+    for (int i = 0; i < board_height; i++)
+      for (int j = 0; j < board_width; j++)
+        obj.push_back(Point3f((float)j * square_size, (float)i * square_size, 0));
+
+    if (found1 && found2) {
+      cout << i << ". Found corners!" << endl;
+      cout<<"left and right rind the cornors at the same time!!!"<<endl;
+      imagePoints1.push_back(corners1);
+      imagePoints2.push_back(corners2);
+      object_points.push_back(obj);
+      imshow("left_image",img1);
+      imshow("right_image",img2);
+      waitKey(100);//暂停0.5S
+    }
+  }
+  for (int i = 0; i < imagePoints1.size(); i++) {
+    vector< Point2f > v1, v2;
+    for (int j = 0; j < imagePoints1[i].size(); j++) {
+      v1.push_back(Point2f((double)imagePoints1[i][j].x, (double)imagePoints1[i][j].y));
+      v2.push_back(Point2f((double)imagePoints2[i][j].x, (double)imagePoints2[i][j].y));
+    }
+    left_img_points.push_back(v1);
+    right_img_points.push_back(v2);
+  }
+}
+
+double computeReprojectionErrors(const vector< vector< Point3f > >& objectPoints,
+                                 const vector< vector< Point2f > >& imagePoints,
+                                 const vector< Mat >& rvecs, const vector< Mat >& tvecs,
+                                 const Mat& cameraMatrix , const Mat& distCoeffs) {
+    vector< Point2f > imagePoints2;
+    int i, totalPoints = 0;
+    double totalErr = 0, err;
+    vector< float > perViewErrors;
+    perViewErrors.resize(objectPoints.size());
+
+    for (i = 0; i < (int)objectPoints.size(); ++i) {
+        projectPoints(Mat(objectPoints[i]), rvecs[i], tvecs[i], cameraMatrix,
+                      distCoeffs, imagePoints2);
+        err = norm(Mat(imagePoints[i]), Mat(imagePoints2), CV_L2);
+        int n = (int)objectPoints[i].size();
+        perViewErrors[i] = (float) std::sqrt(err*err/n);
+        totalErr += err*err;
+        totalPoints += n;
+    }
+    return std::sqrt(totalErr/totalPoints);
+}
+
+int main(int argc, char const *argv[])
+{
+  char* leftcalib_file;
+  char* rightcalib_file;
+  char* leftimg_dir;
+  char* rightimg_dir;
+  char* leftimg_filename;
+  char* rightimg_filename;
+  char* out_file;
+  int num_imgs;
+
+  static struct poptOption options[] = {
+    { "num_imgs",'n',POPT_ARG_INT,&num_imgs,0,"Number of checkerboard images","NUM" },
+    { "leftcalib_file",'u',POPT_ARG_STRING,&leftcalib_file,0,"Left camera calibration","STR" },
+    { "rightcalib_file",'v',POPT_ARG_STRING,&rightcalib_file,0,"Right camera calibration","STR" },
+    { "leftimg_dir",'L',POPT_ARG_STRING,&leftimg_dir,0,"Directory containing left images","STR" },
+    { "rightimg_dir",'R',POPT_ARG_STRING,&rightimg_dir,0,"Directory containing right images","STR" },
+    { "leftimg_filename",'l',POPT_ARG_STRING,&leftimg_filename,0,"Left image prefix","STR" },
+    { "rightimg_filename",'r',POPT_ARG_STRING,&rightimg_filename,0,"Right image prefix","STR" },
+    { "out_file",'o',POPT_ARG_STRING,&out_file,0,"Output calibration filename (YML)","STR" },
+    POPT_AUTOHELP
+    { NULL, 0, 0, NULL, 0, NULL, NULL }
+  };
+
+  POpt popt(NULL, argc, argv, options, 0);
+  int c;
+  while((c = popt.getNextOpt()) >= 0) {}
+
+  FileStorage fsl(leftcalib_file, FileStorage::READ);
+  FileStorage fsr(rightcalib_file, FileStorage::READ);
+
+  load_image_points(fsl["board_width"], fsl["board_height"], num_imgs, fsl["square_size"],
+                   leftimg_dir, rightimg_dir, leftimg_filename, rightimg_filename);
+
+  printf("Starting Calibration\n");
+  Mat K1, K2, R, F, E;
+  Vec3d T;
+  Mat D1, D2;
+  fsl["K"] >> K1;
+  fsr["K"] >> K2;
+  fsl["D"] >> D1;
+  fsr["D"] >> D2;
+  int flag = 0;
+  flag |= CV_CALIB_FIX_INTRINSIC;
+  
+  cout << "Read intrinsics" << endl;
+  
+  stereoCalibrate(object_points, left_img_points, right_img_points, K1, D1, K2, D2, img1.size(), R, T, E, F);
+
+
+
+
+
+  cv::FileStorage fs1(out_file, cv::FileStorage::WRITE);
+  fs1 << "K1" << K1;
+  fs1 << "K2" << K2;
+  fs1 << "D1" << D1;
+  fs1 << "D2" << D2;
+  fs1 << "R" << R;
+  fs1 << "T" << T;
+  fs1 << "E" << E;
+  fs1 << "F" << F;
+  
+  printf("Done Calibration\n");
+
+  printf("Starting Rectification\n");
+
+  cv::Mat R1, R2, P1, P2, Q;
+  Mat mapLx, mapLy, mapRx, mapRy;
+  Rect validROIL, validROIR;
+  Size imageSize = Size(640,360);
+
+  stereoRectify(K1, D1, K2, D2, img1.size(), R, T, R1, R2, P1, P2, Q, CALIB_ZERO_DISPARITY,-1,imageSize,&validROIL,&validROIR);
+
+  fs1 << "R1" << R1;
+  fs1 << "R2" << R2;
+  fs1 << "P1" << P1;
+  fs1 << "P2" << P2;
+  fs1 << "Q" << Q;
+
+
+
+    initUndistortRectifyMap(K1, D1, R1, P1,  img1.size(), CV_32FC1, mapLx, mapLy);
+    initUndistortRectifyMap(K2, D2, R2, P2,  img2.size(), CV_32FC1, mapRx, mapRy);
+
+    /*
+    读取图片
+    */
+    Mat rgbImageL ,rgbImageR;
+    Mat grayImageL,grayImageR;
+    Mat rectifyImageL, rectifyImageR;
+    rgbImageL = imread("/home/laptop2/work_space/stereo-calibration/calib_imgs/1/left1.jpg", CV_LOAD_IMAGE_COLOR);
+    cvtColor(rgbImageL, grayImageL, CV_BGR2GRAY);
+    rgbImageR = imread("/home/laptop2/work_space/stereo-calibration/calib_imgs/1/right1.jpg", CV_LOAD_IMAGE_COLOR);
+    cvtColor(rgbImageR, grayImageR, CV_BGR2GRAY);
+
+    imshow("ImageL Before Rectify", grayImageL);
+    imshow("ImageR Before Rectify", grayImageR);
+
+    /*
+    经过remap之后，左右相机的图像已经共面并且行对准了
+    */
+    remap(grayImageL, rectifyImageL, mapLx, mapLy, INTER_LINEAR);
+    remap(grayImageR, rectifyImageR, mapRx, mapRy, INTER_LINEAR);
+
+    /*
+    把校正结果显示出来
+    */
+    Mat rgbRectifyImageL, rgbRectifyImageR;
+    // Rect validROIL, validROIR;
+
+    cvtColor(rectifyImageL, rgbRectifyImageL, CV_GRAY2BGR);  //伪彩色图
+    cvtColor(rectifyImageR, rgbRectifyImageR, CV_GRAY2BGR);
+
+    //单独显示
+    //rectangle(rgbRectifyImageL, validROIL, Scalar(0, 0, 255), 3, 8);
+    //rectangle(rgbRectifyImageR, validROIR, Scalar(0, 0, 255), 3, 8);
+    imshow("ImageL After Rectify", rgbRectifyImageL);
+    imshow("ImageR After Rectify", rgbRectifyImageR);
+
+    //显示在同一张图上
+    Mat canvas;
+    double sf;
+    int w, h;
+    sf = 600. / MAX(imageSize.width, imageSize.height);
+    w = cvRound(imageSize.width * sf);
+    h = cvRound(imageSize.height * sf);
+    canvas.create(h, w * 2, CV_8UC3);   //注意通道
+
+    //左图像画到画布上
+    Mat canvasPart = canvas(Rect(w * 0, 0, w, h));                                //得到画布的一部分
+    resize(rgbRectifyImageL, canvasPart, canvasPart.size(), 0, 0, INTER_AREA);     //把图像缩放到跟canvasPart一样大小
+    Rect vroiL(cvRound(validROIL.x*sf), cvRound(validROIL.y*sf),                //获得被截取的区域
+               cvRound(validROIL.width*sf), cvRound(validROIL.height*sf));
+    //rectangle(canvasPart, vroiL, Scalar(0, 0, 255), 3, 8);                      //画上一个矩形
+    cout << "Painted ImageL" << endl;
+
+    //右图像画到画布上
+    canvasPart = canvas(Rect(w, 0, w, h));                                      //获得画布的另一部分
+    resize(rgbRectifyImageR, canvasPart, canvasPart.size(), 0, 0, INTER_LINEAR);
+    Rect vroiR(cvRound(validROIR.x * sf), cvRound(validROIR.y*sf),
+               cvRound(validROIR.width * sf), cvRound(validROIR.height * sf));
+    //rectangle(canvasPart, vroiR, Scalar(0, 0, 255), 3, 8);
+    cout << "Painted ImageR" << endl;
+
+    //画上对应的线条
+    for (int i = 0; i < canvas.rows; i += 16)
+        line(canvas, Point(0, i), Point(canvas.cols, i), Scalar(0, 255, 0), 1, 8);
+    imshow("rectified", canvas);
+    cout<<"the size of the img1 is :"<<img1.size()<<endl;
+    waitKey(0);//暂停0.5S
+
+
+
+
+
+
+    printf("Done Rectification\n");
+
+  return 0;
+}
---
+++ stereo-calibration/cmake-build-debug/CMakeFiles/calibrate_stereo.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/laptop2/work_space/stereo-calibration/calib_stereo.cpp" "/home/laptop2/work_space/stereo-calibration/cmake-build-debug/CMakeFiles/calibrate_stereo.dir/calib_stereo.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  "../$(OpenCV_INCLUDE_DIRS)"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
